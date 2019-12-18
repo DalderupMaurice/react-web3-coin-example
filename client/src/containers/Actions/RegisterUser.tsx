66d199b81ceb1c4cfd01d31ae76f33eccb6cd6b7
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2019-12-18 17:30:29 +0100

    chore(app): add different calls

--- client/src/containers/Actions/RegisterUser.tsx
+++ client/src/containers/Actions/RegisterUser.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import userActions from "../../redux/actions/web3Actions"
-import pindaActions from "../../redux/actions/pindaActions"
+import UserActions from "../../redux/actions/web3Actions"
+import PindaActions from "../../redux/actions/pindaActions"
 import useActions from "../../hooks/useActions";
 
 const RegisterUser = React.memo((props: any) => {
   const { data, error, loadedCount, progress, rollbackProgress } = useSelector((state: any) => state.nos.myActionId);
   const second = useSelector((state: any) => state.nos.mySecondAction);
-  const dispatch = useDispatch();
-  const callRegister = useActions(userActions.call);
 
-  console.log("SEDOND ", second)
+  // Optionally to bundle multiple actions
+  const [ userActions, pindaActions ] = useActions([ UserActions, PindaActions ]);
+
+  // Single action mapping
+  const registerAction = useActions(userActions);
 
   return (
     <>
@@ -20,10 +22,15 @@ const RegisterUser = React.memo((props: any) => {
       <h1>Rollback Progress: {rollbackProgress}</h1>
       <h1>Data: {JSON.stringify(data)}</h1>
       <h1>Error: {JSON.stringify(error)}</h1>
-      <button onClick={() => callRegister(`Pingu-${Math.random()}`)}>User Actions mapped</button>
-      <button onClick={() => dispatch(userActions.call(`Pingu-${Math.random()}`))}>User Actions</button>
-      <button onClick={() => dispatch(userActions.reset())}>Reset User Actions</button>
-      <button onClick={() => dispatch(pindaActions.call(`Pingu-${Math.random()}`))}>Pinda Actions</button>
+      <button onClick={() => userActions.call(`Pingu-${Math.random()}`)}>User Actions Multi map call</button>
+      <br />
+      <br />
+      <button onClick={() => registerAction.call(`Pingu-${Math.random()}`)}>Use Action Single map call</button>
+      <button onClick={() => registerAction.cancel()}>Cancel User Actions</button>
+      <button onClick={() => registerAction.reset()}>Reset User Actions</button>
+      <br />
+      <br />
+      <button onClick={() => pindaActions.call(`Pingu-${Math.random()}`)}>Pinda Actions Multi map call</button>
     </>
   );
 });
